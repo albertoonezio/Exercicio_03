a6ea0b0d1ac3248b55474cc7d9f2f3fef66d0c0a
Alberto <albertomaronezio@hotmai.com>
2019-05-29 12:37:02 -0300

    Exercicio_Parte2_03

---
+++ Exercicio_Parte2_03/Exercicio_Parte2_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            int resultadoDivisao = 0;
+
+            Console.Write("Digite o Primeiro Número: ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Segundo Número: ");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+            resultadoDivisao = primeiroNumero / segundoNumero;
+
+            Console.WriteLine("A Divisão Total é: {0}", resultadoDivisao);
+        }
+    }
+}
